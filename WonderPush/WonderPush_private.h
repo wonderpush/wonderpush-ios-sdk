1653cb9dc59d43bf7b70c1a5f03926ab00c0fe0e
Olivier Favre <olivier@wonderpush.com>
2016-06-14 12:34:28 +0200

    Release v1.2.1.0

--- WonderPush/WonderPush_private.h
+++ WonderPush/WonderPush_private.h
@@ -21,7 +21,7 @@
 #import "WPResponse.h"
 
 
-#define SDK_VERSION @"iOS-1.2.0.1"
+#define SDK_VERSION @"iOS-1.2.1.0"
 #define PRODUCTION_API_DOMAIN @"api.wonderpush.com"
 #define PRODUCTION_API_URL @"https://" PRODUCTION_API_DOMAIN @"/v1/"
 
