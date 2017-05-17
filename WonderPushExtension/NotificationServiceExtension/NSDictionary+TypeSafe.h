108fbf2d535ae55f7f081d77cc691053af0c7af5
Olivier Favre <olivier@wonderpush.com>
2017-05-17 16:00:02 +0200

    Symlinks fail pod trunk validation

--- WonderPushExtension/NotificationServiceExtension/NSDictionary+TypeSafe.h
+++ WonderPushExtension/NotificationServiceExtension/NSDictionary+TypeSafe.h
@@ -1,1 +1,21 @@
-../../WonderPush/NSDictionary+TypeSafe.h
+//
+//  NSDictionary+TypeSafe.h
+//  WonderPush
+//
+//  Created by Olivier Favre on 11/07/16.
+//  Copyright © 2016 WonderPush. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (TypeSafe)
+
+- (id) typesafeObjectForKey:(id)key expectClass:(Class)expectedClass;
+- (id) nullsafeObjectForKey:(id)key;
+
+- (NSDictionary *) dictionaryForKey:(id)key;
+- (NSArray *) arrayForKey:(id)key;
+- (NSString *) stringForKey:(id)key;
+- (NSNumber *) numberForKey:(id)key;
+
+@end
--- WonderPushExtension/NotificationServiceExtension/NSDictionary+TypeSafe.m
+++ WonderPushExtension/NotificationServiceExtension/NSDictionary+TypeSafe.m
@@ -1,1 +1,52 @@
-../../WonderPush/NSDictionary+TypeSafe.m
+//
+//  NSDictionary+TypeSafe.m
+//  WonderPush
+//
+//  Created by Olivier Favre on 11/07/16.
+//  Copyright © 2016 WonderPush. All rights reserved.
+//
+
+#import "NSDictionary+TypeSafe.h"
+
+@implementation NSDictionary (TypeSafe)
+
+- (id) typesafeObjectForKey:(id)key expectClass:(Class)expectedClass
+{
+    id value = self[key];
+    if ([value isKindOfClass:expectedClass]) {
+        return value;
+    }
+    return nil;
+}
+
+- (id) nullsafeObjectForKey:(id)key
+{
+    id value = self[key];
+    if (value != [NSNull null]) {
+        return value;
+    }
+    return nil;
+}
+
+
+- (NSDictionary *) dictionaryForKey:(id)key
+{
+    return [self typesafeObjectForKey:key expectClass:[NSDictionary class]];
+}
+
+- (NSArray *) arrayForKey:(id)key
+{
+    return [self typesafeObjectForKey:key expectClass:[NSArray class]];
+}
+
+- (NSString *) stringForKey:(id)key
+{
+    return [self typesafeObjectForKey:key expectClass:[NSString class]];
+}
+
+- (NSNumber *) numberForKey:(id)key
+{
+    return [self typesafeObjectForKey:key expectClass:[NSNumber class]];
+}
+
+@end
