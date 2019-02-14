cfee66f10fda46dcc4ee6dff3e0e22aa96dfed0b
Stéphane JAIS <stephane@wonderpush.com>
2019-02-14 15:38:14 +0100

    new constant

--- WonderPush/WPUtil.m
+++ WonderPush/WPUtil.m
@@ -29,6 +29,7 @@ NSString * const WPErrorDomain = @"WPErrorDomain";
 NSInteger const WPErrorInvalidCredentials = 11000;
 NSInteger const WPErrorInvalidAccessToken = 11003;
 NSInteger const WPErrorMissingUserConsent = 11011;
+NSInteger const WPErrorHTTPFailure = 11012;
 @implementation WPUtil
 
 + (NSString*)base64forData:(NSData*)theData
