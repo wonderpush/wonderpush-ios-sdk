64d8c8f62c883422630059859434bc1f163be523
Stéphane JAIS <stephane@wonderpush.com>
2019-02-20 19:42:20 +0100

    Using WKWebView

--- WonderPush/Sources/WPHTMLInAppController.m
+++ WonderPush/Sources/WPHTMLInAppController.m
@@ -6,23 +6,25 @@
 //  Copyright © 2019 WonderPush. All rights reserved.
 //
 
+#import <WebKit/WebKit.h>
 #import "WPHTMLInAppController.h"
 #import "WPLog.h"
 
-@interface WPHTMLInAppController () <UIWebViewDelegate>
+@interface WPHTMLInAppController () <WKNavigationDelegate>
 @property (weak, nonatomic) IBOutlet UIStackView *buttonStackView;
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property (weak, nonatomic) IBOutlet UIView *titleLabelContainerView;
-@property (weak, nonatomic) IBOutlet UIWebView *webView;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *webViewWidthConstraint;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *webViewHeightConstraint;
+@property (weak, nonatomic) IBOutlet UIStackView *mainStackView;
+@property (strong, nonatomic) IBOutlet WKWebView *webView;
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *webViewWidthConstraint;
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *webViewHeightConstraint;
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
 @property (weak, nonatomic) IBOutlet UIView *containerView;
 @property (strong, nonatomic) NSArray<UIButton*> *buttons;
 @property (assign, nonatomic) BOOL loaded;
 @end
 
-static NSTimeInterval timeout = 10;
+static NSTimeInterval timeout = 60;
 @implementation WPHTMLInAppAction
 + (instancetype) actionWithTitle:(NSString *)title block:(void (^)(WPHTMLInAppAction *))block
 {
@@ -37,7 +39,18 @@ static NSTimeInterval timeout = 10;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.webView.delegate = self;
+    WKWebViewConfiguration *configuration = [WKWebViewConfiguration new];
+    configuration.allowsInlineMediaPlayback = YES;
+    self.webView = [[WKWebView alloc] initWithFrame:CGRectZero configuration:configuration];
+    self.webView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.webViewWidthConstraint = [NSLayoutConstraint constraintWithItem:self.webView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:0 constant:1000];
+    self.webViewWidthConstraint.priority = 500;
+    self.webViewHeightConstraint = [NSLayoutConstraint constraintWithItem:self.webView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:0 constant:1000];
+    self.webViewHeightConstraint.priority = 500;
+    [self.webView addConstraints:@[self.webViewWidthConstraint, self.webViewHeightConstraint]];
+    [self.mainStackView addArrangedSubview:self.webView];
+    
+    self.webView.navigationDelegate = self;
     self.titleLabel.text = self.title;
     self.titleLabelContainerView.hidden = !([self.titleLabel.text length]);
     if (self.URL) {
@@ -67,6 +80,12 @@ static NSTimeInterval timeout = 10;
         }
     });
 }
+
+- (void)dealloc
+{
+    [self.webView.scrollView removeObserver:self forKeyPath:@"contentSize" context:nil];
+}
+
 - (void) buttonClicked:(id)sender
 {
     UIView *senderView = sender;
@@ -90,20 +109,32 @@ static NSTimeInterval timeout = 10;
 }
 */
 
-- (void) webViewDidFinishLoad:(UIWebView *)webView
+
+- (void)observeValueForKeyPath:(NSString *)keyPath
+                      ofObject:(id)object
+                        change:(NSDictionary *)change
+                       context:(void *)context
 {
-    self.loaded = YES;
-    CGSize contentSize = webView.scrollView.contentSize;
-    self.webViewWidthConstraint.constant = contentSize.width;
-    self.webViewHeightConstraint.constant = contentSize.height;
-    [self.view layoutIfNeeded];
-    [self.activityIndicator stopAnimating];
-    [UIView animateWithDuration:0.4 delay:0 usingSpringWithDamping:0.6 initialSpringVelocity:0.3 options:0 animations:^{
-        self.containerView.transform = CGAffineTransformIdentity;
-    } completion:nil];
+    if (object == self.webView.scrollView && [keyPath isEqual:@"contentSize"]) {
+        CGSize contentSize = self.webView.scrollView.contentSize;
+        self.webViewWidthConstraint.constant = contentSize.width;
+        self.webViewHeightConstraint.constant = contentSize.height;
+        [self.view layoutIfNeeded];
+        [self.activityIndicator stopAnimating];
+        [UIView animateWithDuration:0.4 delay:0 usingSpringWithDamping:0.6 initialSpringVelocity:0.3 options:0 animations:^{
+            self.containerView.transform = CGAffineTransformIdentity;
+        } completion:nil];
+    }
 }
 
-- (void) webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
+- (void) webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation
+{
+    if (!self.loaded) {
+        self.loaded = YES;
+        [self.webView.scrollView addObserver:self forKeyPath:@"contentSize" options:NSKeyValueObservingOptionNew context:nil];
+    }
+}
+- (void) webView:(WKWebView *)webView didFailNavigation:(WKNavigation *)navigation withError:(NSError *)error
 {
     self.loaded = YES;
     WPLog(@"Could not load in-app website: %@", error);
