440caa49c64b94f75f1e632d913b44e31a041619
Stéphane JAIS <stephane@wonderpush.com>
2020-03-24 10:16:54 +0100

    WPURLConstants file

---
+++ WonderPush/Sources/WPURLConstants.h
@@ -0,0 +1,4 @@
+#define WP_TARGET_URL_SDK_PREFIX @"wonderpush://"
+#define WP_TARGET_URL_DEFAULT @"wonderpush://notificationOpen/default"
+#define WP_TARGET_URL_NOOP @"wonderpush://notificationOpen/noop"
+#define WP_TARGET_URL_BROADCAST @"wonderpush://notificationOpen/broadcast"
