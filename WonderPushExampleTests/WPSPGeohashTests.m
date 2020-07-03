a6478167bf4a8ea9ccbf87db774d0a370bc4b966
Stéphane JAIS <stephane@wonderpush.com>
2020-07-03 14:49:54 +0200

    remove boilerplate code

--- WonderPushExampleTests/WPSPGeohashTests.m
+++ WonderPushExampleTests/WPSPGeohashTests.m
@@ -82,12 +82,4 @@
     XCTAssertEqualWithAccuracy(bottomRight.lat, self.bottom, self.delta);
     XCTAssertEqualWithAccuracy(bottomRight.lon, self.right, self.delta);
 }
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
 @end
