090ed44e7dc7d39dfeb6d63513c7d0e98e0874a4
Stéphane JAIS <stephane@wonderpush.com>
2020-10-12 15:44:35 +0200

    extract defined constants in their own file

--- WonderPush/Sources/WonderPush_private.h
+++ WonderPush/Sources/WonderPush_private.h
@@ -25,68 +25,7 @@
 #import "WPRemoteConfig.h"
 #import "WPMeasurementsApiClient.h"
 
-#define SDK_VERSION @"iOS-3.3.3"
-#define PRODUCTION_API_DOMAIN @"api.wonderpush.com"
-#define PRODUCTION_API_URL @"https://" PRODUCTION_API_DOMAIN @"/v1/"
-#define REMOTE_CONFIG_BASE_URL @"https://cdn.by.wonderpush.com/config/clientids/"
-#define REMOTE_CONFIG_SUFFIX @"-iOS"
-
-#define RETRY_INTERVAL 10.0f
-#define CACHED_INSTALLATION_CUSTOM_PROPERTIES_MIN_DELAY 5
-#define CACHED_INSTALLATION_CUSTOM_PROPERTIES_MAX_DELAY 20
-#define CACHED_INSTALLATION_CORE_PROPERTIES_MIN_DELAY 5
-#define CACHED_INSTALLATION_CORE_PROPERTIES_MAX_DELAY 20
-
-#define ITUNES_APP_URL_FORMAT @"https://itunes.apple.com/us/app/calcfast/id%@?mt=8"
-#define WEB_CALLBACK_RESOURCE @"web/callback"
-
-#define DIFFERENT_SESSION_REGULAR_MIN_TIME_GAP      (30*60*1000)
-#define DIFFERENT_SESSION_NOTIFICATION_MIN_TIME_GAP (15*60*1000)
-
-#define PRESENCE_UPDATE_SAFETY_MARGIN 60
-#define PRESENCE_ANTICIPATED_TIME (30*60)
-
-/**
- Key to set in your .plist file to allow rating button action
- */
-#define WP_ITUNES_APP_ID @"itunesAppID"
-
-
-/**
- Key of the WonderPush content in a push notification
- */
-#define WP_PUSH_NOTIFICATION_KEY @"_wp"
-
-/**
- Key of the notification type in the WonderPush content of a push notification
- */
-#define WP_PUSH_NOTIFICATION_TYPE_KEY @"type"
-
-/**
- Key of the deep link url to open with the notification
- */
-#define WP_TARGET_URL_KEY @"targetUrl"
-
-/**
- Notification of type url
- */
-#define WP_PUSH_NOTIFICATION_SHOW_URL @"url"
-
-/**
- Notification of type text
- */
-#define WP_PUSH_NOTIFICATION_SHOW_TEXT @"text"
-
-/**
- Notification of type html
- */
-#define WP_PUSH_NOTIFICATION_SHOW_HTML @"html"
-
-/**
- Notification of type data
- */
-#define WP_PUSH_NOTIFICATION_DATA @"data"
-
+#import "WonderPush_constants.h"
 
 /**
  Default notification button label
