4b07df1b542ea03ac6643e2ab81a87be26ce0465
Stéphane JAIS <stephane@wonderpush.com>
2021-12-15 17:53:28 +0100

    Angled bracket imports in Framework headers

--- Sources/WonderPush/InAppMessaging.h
+++ Sources/WonderPush/InAppMessaging.h
@@ -14,5 +14,5 @@
  * limitations under the License.
  */
 
-#import "WPInAppMessaging.h"
-#import "WPInAppMessagingRendering.h"
+#import <WonderPush/WPInAppMessaging.h>
+#import <WonderPush/WPInAppMessagingRendering.h>
