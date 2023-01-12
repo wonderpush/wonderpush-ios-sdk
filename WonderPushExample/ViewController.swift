6acb38f711c58134979920dd036ee3f34a979a2d
Olivier Favre <olivier@wonderpush.com>
2023-01-12 10:44:33 +0100

    Send some event for representing the Live Acitivity state

--- WonderPushExample/ViewController.swift
+++ WonderPushExample/ViewController.swift
@@ -13,6 +13,7 @@ import WonderPush
 
 class ViewController: UIViewController {
     var activity: Activity<WonderPushWidgetExtensionAttributes>?
+    let formatter = ISO8601DateFormatter()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,14 +26,20 @@ class ViewController: UIViewController {
         let activityAttributes = WonderPushWidgetExtensionAttributes(name:"some name")
         do {
             activity = try Activity.request(attributes: activityAttributes, contentState: initialContentState, pushType: .token)
-            print("Requested a Live Activity \(String(describing: activity?.id)).")
-            if let updates = activity?.pushTokenUpdates, let id = activity?.id {
+            if let activity = activity {
+                print("Requested a Live Activity \(String(describing: activity.id)).")
+                if let pushToken = activity.pushToken {
+                    WonderPush.trackEvent("NewLiveActivity", attributes: [
+                        "string_liveActivityId": activity.id,
+                        "ignore_liveActivityPushToken": WPNSUtil.hex(for: pushToken),
+                        "date_liveActivityExpiration": formatter.string(from: Date().addingTimeInterval(3600 * 8))
+                    ])
+                }
                 Task {
-                    let formatter = ISO8601DateFormatter()
-                    for await update in updates {
+                    for await update in activity.pushTokenUpdates {
                         WonderPush.trackEvent("NewLiveActivity", attributes: [
+                            "string_liveActivityId": activity.id,
                             "ignore_liveActivityPushToken": WPNSUtil.hex(for: update),
-                            "string_liveActivityId": id,
                             "date_liveActivityExpiration": formatter.string(from: Date().addingTimeInterval(3600 * 8))
                         ])
                     }
@@ -49,6 +56,7 @@ class ViewController: UIViewController {
             let updatedContentState = WonderPushWidgetExtensionAttributes.ContentState(value: value, name: "updated")
             let alertConfiguration = AlertConfiguration(title: "Activity Update", body: "Value has been updated to \(value)", sound: .default)
 
+            print("Updating a Live Activity \(String(describing: activity?.id)).")
             await activity?.update(using: updatedContentState, alertConfiguration: alertConfiguration)
         }
     }
@@ -57,7 +65,14 @@ class ViewController: UIViewController {
         let finalContentState = WonderPushWidgetExtensionAttributes.ContentState(value: 789, name: "ended")
 
         Task {
-            await activity?.end(using:finalContentState, dismissalPolicy: .default)
+            if let activity = activity {
+                print("Stopping a Live Activity \(String(describing: activity.id)).")
+                await activity.end(using:finalContentState, dismissalPolicy: .default)
+                WonderPush.trackEvent("NewLiveActivity", attributes: [
+                    "string_liveActivityId": activity.id,
+                    "date_liveActivityExpiration": 0
+                ])
+            }
         }
     }
     
